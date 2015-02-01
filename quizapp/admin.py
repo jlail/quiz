fde3a426e4d81d8e3acc61181fa714c99b61ee4e
justin <justin.lail@hotmail.com>
2015-02-01 12:27:29 -0700

    update model and admin console to allow question/answer input

--- quizapp/admin.py
+++ quizapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from quizapp.models import Question, Answer
 
-# Register your models here.
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+admin.site.register(Question, QuestionAdmin)
--- quizapp/models.py
+++ quizapp/models.py
@@ -2,15 +2,20 @@ from django.db import models
 
 
 class Question(models.Model):
-    answer_object = models.ForeignKey('Answer')
     question_text = models.TextField()
     question_category = models.CharField(max_length=200)
     previous_attempt_correct = models.BooleanField(default=False)
     number_of_answers = models.IntegerField()
 
+    def __str__(self):
+        return self.question_text
+
 
 class Answer(models.Model):
-    question_object = models.ForeignKey('Question')
+    question = models.ForeignKey(Question)
     answer_text = models.TextField()
     answer_category = models.CharField(max_length=200)
     number_of_answers = models.IntegerField()
+
+    def __str__(self):
+        return self.answer_text
