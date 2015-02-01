ecbbd25e9d5fed78777cd33ad37aec58f0b57b4a
justin <justin.lail@hotmail.com>
2015-02-01 12:42:23 -0700

    added to db and updated admin console

--- quizapp/admin.py
+++ quizapp/admin.py
@@ -9,6 +9,9 @@ class AnswerInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
+    list_display = ('question_text', 'question_category')
+    list_filter = ['question_category', 'number_of_answers']
+    search_fields = ['question_text', 'question_category']
 
 
 admin.site.register(Question, QuestionAdmin)
